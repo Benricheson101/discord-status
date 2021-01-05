ebd8f2792faad10566c429bdab14d9a9bd0e22d3
B <benricheson101@gmail.com>
2021-01-05 03:11:27 -0500

    url

--- src/util/embeds/Base.ts
+++ src/util/embeds/Base.ts
@@ -10,7 +10,9 @@ export abstract class BaseEmbed extends EmbedBuilder {
   author = {
     name: 'Discord Status',
     icon_url: 'https://discord.com/assets/2c21aeda16de354ba5334551a883b481.png',
-    url: 'https://s.red-panda.red/discord', // TODO: pull from config
+    url: global.config.meta?.support_server
+      ? `https://discord.gg/${global.config.meta?.support_server}`
+      : '',
   };
 
   constructor(public incident: Incident) {
